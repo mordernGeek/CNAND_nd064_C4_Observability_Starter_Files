868d4be9e7f86e577881525d59cdc3ec5a4f3ad0
mordern_geek <dyopal@gmail.com>
2021-11-30 08:19:09 +0100

    tracing...

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request
 from jaeger_client import Config
 from flask_opentracing import FlaskTracing
+from opentracing_instrumentation.client_hooks import install_all_patches
 
 app = Flask(__name__)
+# creating my configuration object for all requests
+logging.get.Logger('').handlers = []
+logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 config = Config(
     config={
         'sampler':
@@ -10,15 +14,27 @@ config = Config(
          'param': 1},
                         'logging': True,
                         'reporter_batch_size': 1,}, 
-                        service_name="service")
+                        service_name="frontend")
 jaeger_tracer = config.initialize_tracer()
 tracing = FlaskTracing(jaeger_tracer, True, app)
 
+test()
 
 @app.route("/")
 def homepage():
     return render_template("main.html")
 
+def test():
+    # Adding the span feature...
+     with tracer.start_span('frontSpan') as span:
+        req = requests.get('https://147.182.198.136:3000.com') # ping my own grafana
+        span.set_tag('http.method;', req)
+        def format():
+            with tracer.start_span('front-span') as span:
+                try:
+                    print("started tracing!")
+                except:
+                    print("start jaeger tracing!")
 
 if __name__ == "__main__":
     app.run()
